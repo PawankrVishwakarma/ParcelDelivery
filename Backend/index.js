3bc9d8d475c1de564cc5b471d47605f5ad463555
PAWAN KUMAR VISHWAKARMA <pawankumarvishwakarma@Pawans-MacBook-Air.local>
2024-10-04 02:49:10 +0530

    Authentication

---
+++ Backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutue = require("./routes/auth");
+
+require('dotenv').config();
+const app = express();
+
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+
+
+// ROUTES
+
+app.use("/auth", authRoute)
+
+
+// DARABASE CONNECTION
+const DB = process.env.DB
+mongoose.connect(DB).then(()=>{
+    console.log("DB connection is successful")
+}).catch((err)=>{
+    console.log(err) 
+ })
+
+
+
+// SERVER
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`)
+})
